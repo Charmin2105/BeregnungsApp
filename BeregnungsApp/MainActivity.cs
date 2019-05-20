8d13b09ace02e211abbec4dc8f1dce8810468c82
Nico Herrmann <ni.herrmann@ostfalia.de>
2019-05-20 13:31:01 +0200

    Projektdateien hinzufügen.

---
+++ BeregnungsApp/MainActivity.cs
@@ -0,0 +1,45 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System;
+using Android.Content;
+
+namespace BeregnungsApp
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        Button buttonLogin;
+        EditText editTextEmail;
+        EditText editTextPassword;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            //EditText initalisieren
+             editTextEmail = FindViewById<EditText>(Resource.Id.editTextMainMail);
+            editTextPassword = FindViewById<EditText>(Resource.Id.editTextMainPassword);
+
+            // Button initalisieren
+            buttonLogin = FindViewById<Button>(Resource.Id.buttonMainLogin);
+            buttonLogin.Click += OnButtonLoginClicked;
+        }
+        public void OnButtonLoginClicked(object sender, EventArgs e)
+        {
+            var mail = editTextEmail.Text.ToString();
+            var password =  editTextPassword.Text.ToString();
+            Login(mail, password);
+            Intent intent = new Intent(this, typeof(SecondActivity));
+            StartActivity(intent);
+        }
+        private void Login(string mail, string password)
+        {
+
+        }
+    }
+}
